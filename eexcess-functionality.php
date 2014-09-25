6035dc09492a89a1863aad4c815a57e50dba491d
Nils Witt <n.witt@zbw.eu>
2014-09-25 11:34:56 +0200

    [feature] "get recommendations"-button added to WYSIWYG-editor
    A custom "get recommendations"-button has been added to the tinyMCE.

--- eexcess-functionality.php
+++ eexcess-functionality.php
@@ -230,4 +230,32 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
       }";
       return $initArray;
    }
+
+   //ADDING A BUTTON TO tinyMCE
+   add_action( 'admin_head', 'EEXCESS_add_tinymce' );
+   function EEXCESS_add_tinymce() {
+       global $typenow;
+
+       // only on Post Type: post and page
+       if( ! in_array( $typenow, array( 'post', 'page' ) ) )
+           return ;
+
+       add_filter( 'mce_external_plugins', 'EEXCESS_add_tinymce_plugin' );
+       // Add to line 1 form WP TinyMCE
+       add_filter( 'mce_buttons', 'EEXCESS_add_tinymce_button' );
+   }
+
+   // inlcude the js for tinymce
+   function EEXCESS_add_tinymce_plugin( $plugin_array ) {
+
+       $plugin_array['fb_test'] = plugins_url( 'js/tinyMCE_Custom_Button.js', __FILE__ );
+       return $plugin_array;
+   }
+
+   // Add the button key for address via JS
+   function EEXCESS_add_tinymce_button( $buttons ) {
+
+       array_push( $buttons, 'Get_Recommendations_Button');
+       return $buttons;
+   }
 ?>
