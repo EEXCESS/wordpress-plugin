91a9f9658b9e016354a906273f91f4d533c39da5
Nils Witt <n.witt@zbw.eu>
2015-06-10 10:41:51 +0200

    Minor changes

--- js/CLSWrapper.js
+++ js/CLSWrapper.js
@@ -3,7 +3,7 @@ $j = jQuery.noConflict();
 
 /*
  * The Citation Processor is a wrapper for the citeproc-js library.
- * It simplifies the use of citeproc-js and is implemented in 
+ * It simplifies the use of citeproc-js and is implemented in
  * pseudo-class style.
  */
 var CITATION_PROCESSOR = function(){
@@ -11,20 +11,20 @@ var CITATION_PROCESSOR = function(){
    style,
    citations,
    citeprocSys;
-   
+
    /*
     * The init-function is responsible for the communication with the
     * outer world.
-    * @param locals: the path to a location xml-file. example: 
+    * @param locals: the path to a location xml-file. example:
     * https://bitbucket.org/fbennett/citeproc-js/src/01429717257da70cb8cef6dccbde51fdf6fa763d/demo/locales-en-US.xml?at=default
     * @param style: the path to a style csl-file. >7.000 file can be found here: https://zotero.org/styles
-    * @param citations: A JSON-like object containing the information to be cited. 
+    * @param citations: A JSON-like object containing the information to be cited.
     */
    init = function(locals, style, citations){
       this.locals = getFile(locals);
       this.style = getFile(style);
       this.citations = citations;
-      
+
       // Initialize a system object, which contains two methods needed by the
       // engine.
       citeprocSys = {
@@ -38,14 +38,14 @@ var CITATION_PROCESSOR = function(){
              return this.context.citations[id];
          }
       };
-   }   
-   
+   }
+
    /*
     * Fetches and returns the content of a file. Since javascript is not able
-    * to handle files natively, this functions relies on a webserver to server
-    * the file. Therefor the root of the path corresponds to the webservers root. 
+    * to handle files natively, this functions relies on a webserver to serve
+    * the file. Therefore the root of the path corresponds to the webservers root.
     * @param path: a string containing the path to file to be opened.
-    */ 
+    */
    getFile = function(path){
       var request = $j.ajax({
          type: "GET",
@@ -58,20 +58,20 @@ var CITATION_PROCESSOR = function(){
          return null;
       }
    },
-   
+
    /*
     * Given the identifier of a CSL style, this function instantiates a CSL.Engine
     * object that can render citations in that style.
-    * @param context: context must contain a serialized csl-file. 
-    */ 
+    * @param context: context must contain a serialized csl-file.
+    */
    getProcessor = function(context) {
-      var citeproc = null;   
+      var citeproc = null;
       if(context.style != null){
          citeproc = new CSL.Engine(citeprocSys, context.style);
       }
       return citeproc;
    };
-   
+
    /*
     * This renders the citation-object and returs it. Thus, it is, beside the init-
     * function, the only publicly-accessable function. It returns an HTML-containing
@@ -89,7 +89,7 @@ var CITATION_PROCESSOR = function(){
          return bibResult[1].join('\n');
       }
    };
-   
+
    return{
       init: init,
       renderCitations: renderCitations,
