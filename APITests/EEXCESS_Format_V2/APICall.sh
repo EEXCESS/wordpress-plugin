fc70a925b124b2fea78f74a56ded77f4695051b2
Nils Witt <n.witt@zbw.eu>
2015-07-03 10:05:40 +0200

    Enhanced new exchange formats implementation

---
+++ APITests/EEXCESS_Format_V2/APICall.sh
@@ -0,0 +1,22 @@
+recommendURL="http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/recommend"
+getdetailsURL="http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/getDetails"
+
+if [ "$2" == "recommend" ]; then
+   url=$recommendURL
+else
+   if [ "$2" == "details" ]; then
+      url=$getdetailsURL
+   else
+      url=$recommendURL
+   fi
+fi
+
+if [ -f "$1" ]; then
+   curl -v -H "Accept: application/json" -H "Content-Type: application/json" -d @"$1" "$url" 2>/dev/null | python -m json.tool 
+else
+   echo "
+Usage: APICall FILENAME [METHOD] 
+FILENAME:   A valid JSON file
+METHOD:     Optional parameter. Determines the method. 'recommend' and 'details' are valid options.
+"
+fi
