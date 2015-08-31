8817d7dbec0e51bbacf13f909fa044c8bb370325
Nils Witt <n.witt@zbw.eu>
2015-08-31 12:16:28 +0200

    Support for "add Image" button added in SearchResultList

--- js/resultList.js
+++ js/resultList.js
@@ -1,7 +1,15 @@
 // load dependencies
-require(['jquery', 'APIconnector', 'iframes', 'citationBuilder'], function($, api, iframes, citationBuilder) {
+require(['jquery', 'APIconnector', 'iframes', 'citationBuilder', 'eexcessMethods'], function($, api, iframes, citationBuilder, eexcessMethods) {
     // set the URL of the federated recommender to the stable server
     // api.init({url: 'http://eexcess.joanneum.at/eexcess-privacy-proxy/api/v1/recommend'});
+
+
+   eexcessMethods = eexcessMethods($("#eexcess_container .inside #content .eexcess-spinner"),
+            $("#eexcess_container .inside #content #list"),
+            $("#eexcess_container .inside #content p"),
+            $('#abortRequest'),
+            $('#citationStyleDropDown'),
+            $('#searchQueryReflection'));
 
     // helper method to avoid duplicated code
     var query_PP = function(profile) {
@@ -111,25 +119,64 @@ require(['jquery', 'APIconnector', 'iframes', 'citationBuilder'], function($, ap
                    citationBuilder.addAsCitation(record);
                 } else if(response.status === 'error'){
                    alert("Could not retrieve data required to asseble the citation");
-                } else{
+                } else {
 
                 }
             });
         }
+
+        /*
+         * This signal is sent after the user hit a citation as image button.
+         */
+        if (msg.data.event && msg.data.event === 'eexcess.imageCitationRequest') {
+           var imageURL = msg.data.documentsMetadata.previewImage,
+           title = msg.data.documentsMetadata.title,
+           snippet = "<a title='" + title + "' href='" + imageURL + "' target='_blank'><img src='" + imageURL + "'/></a>",
+           position = eexcessMethods.getCursor(),
+           content = eexcessMethods.getContent();
+
+           if(eexcessMethods.extendedLoggingEnabled()){
+              try{
+                 sendUsersActivitiesSignal("image_embedded", this);
+              }catch(e){
+                 console.log("Logging failed. Message was: " + e.message);
+              }
+           }
+
+           var insertionPosition = eexcessMethods.determineDecentInsertPosition.call(eexcessMethods, content, position);
+           var newText = insertIntoText(content, insertionPosition, snippet);
+           eexcessMethods.setContent(newText);
+        }
+
+        /*
+         * Registers custom buttons after the iframe has signaled, that the msg listeners are in place
+         */
+        if (msg.data.event && msg.data.event === 'eexcess.msgListenerLoaded') {
+           // registers buttons if iframe loads second
+           registerButtons();
+        }
     };
-    
+    // registers buttons if iframe loads first
+    registerButtons();
+
     /*
      * When the iframe is completly loaded, send a message to add buttons
      */
-    //var iframe = $("#resultList")[0].contentDocument;
-    var iframe = $("#resultList")[0];
-    $(iframe).ready(function(){
-       iframes.sendMsg({
-           event: 'eexcess.registerButton.perResult',
-           node: '<body>',
-           html: '<div style="float: right; padding-top: 6px;"><img src="' + plugin_url + 'images/Sketch-Book-icon.png' + '"></div>',
-           responseEvent: 'eexcess.citationRequest'
+    function registerButtons(){
+        // cite as citation button
+        iframes.sendMsg({
+            event: 'eexcess.registerButton.perResult',
+            html: '<div style="float: right; padding-top: 6px;"><img alt="Cite as citation" src="' + plugin_url + 'images/Sketch-Book-icon.png' + '"></div>',
+            responseEvent: 'eexcess.citationRequest'
         }, 
         ['resultList']);
-    });
+         
+        // cite as image button
+        iframes.sendMsg({
+            event: 'eexcess.registerButton.perResult',
+            html: '<div style="float: right; padding-top: 6px;"><img alt="Cite as image" src="' + plugin_url + 'images/Sketch-Book-icon.png' + '"></div>',
+            responseEvent: 'eexcess.imageCitationRequest'
+        }, 
+        ['resultList']);
+    }
 });
